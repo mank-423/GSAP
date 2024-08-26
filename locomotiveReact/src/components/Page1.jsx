9d00996eb9bbfef126d3072418004e86075c7398
Mayank Kumar <mk4664348@gmail.com>
2024-08-27 00:01:47 +0530

    Locomotive implementation

---
+++ locomotiveReact/src/components/Page1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Page1 = () => {
+  return (
+    <div>
+      <div className="page">
+        <h1>Page</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Page1
